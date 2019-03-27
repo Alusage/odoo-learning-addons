23cf192b28e090e7a852f29f13d8ea7f751b1f16
default <default@debian9erp>
2019-03-27 14:02:49 +0100

    [FIX] odoo bug with onchange

--- training_base/models/sale_order.py
+++ training_base/models/sale_order.py
@@ -31,3 +31,19 @@ class SaleOrder(models.Model):
                                                    add_qty=add_qty,
                                                    set_qty=set_qty,
                                                    **kwargs)
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    @api.onchange('event_ticket_id')
+    def _onchange_event_ticket_id(self):
+        _logger.debug("############## ONCHANGE #################")
+        super(SaleOrderLine, self)._onchange_event_ticket_id()
+        _logger.debug("In onchange")
+        company = self.event_id.company_id or self.env.user.company_id
+        currency = company.currency_id
+        self.price_unit = currency._convert(
+            self.event_ticket_id.price, self.order_id.currency_id, self.order_id.company_id, self.order_id.date_order or fields.Date.today())
+        _logger.debug("price: %s" % self.price_unit)
+        # we call this to force update the default name
+        _logger.debug("price: %s" % self.price_unit)
